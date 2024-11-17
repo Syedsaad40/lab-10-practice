d88ce4699501bdee2e6a963afbeec3491550ba33
Syedsaad40 <mosyedsaad11@gmail.com>
2024-11-17 23:35:08 +0500

    q1.c

--- lab-10 practice/q1.c
+++ lab-10 practice/q1.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-
-int calculateWeight(int* boxes, int n) {
-    int totalWeight = 0;
-    for (int i = 0; i < n; i++) {
-        totalWeight += boxes[i];
+#include <stdlib.h>
+int calculate_total_weight(void* box[]) {
+    int total_weight = 0;
+    for (int i = 0; box[i] != NULL; i++) {
+        if (*(int*)box[i] >= 0) {
+            total_weight += *(int*)box[i];
+        } else {
+            total_weight += calculate_total_weight((void**)box[i]);
+        }
     }
-    return totalWeight;
+    return total_weight;
 }
 
 int main() {
-    int boxes[] = {5, 10, 15};  // example weights
-    int n = sizeof(boxes) / sizeof(boxes[0]);
-    printf("Total weight: %d\n", calculateWeight(boxes, n));
+    int item1 = 5, item2 = 10, item3 = 2, item4 = 3;
+    void* nestedBox1[] = {(void*)&item1, (void*)&item2, NULL}; 
+    void* nestedBox2[] = {(void*)&item3, (void*)&item4, (void*)nestedBox1, NULL};
+    void* mainBox[] = {(void*)&item1, (void*)nestedBox2, (void*)&item2, NULL}; 
+    int total_weight = calculate_total_weight(mainBox);
+    printf("Total weight: %d\n", total_weight);
+
     return 0;
 }
